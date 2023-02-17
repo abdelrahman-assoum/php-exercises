a2a3e2c6e8c9ff5edc3fcfac910e9193b9f6daaa
abdelrahman <abdelrahman.assoum@gmail.com>
2023-02-17 18:13:02 +0200

    finish step 3

---
+++ php-basics-01/Step_3.php
@@ -0,0 +1,9 @@
+<?php
+$name = "abdelrahman";
+$email = "abdelrahman.assoum@gmail.com";
+echo "my name is ". $name . " and my email is " . $email;
+$height = 174;
+$width = 50;
+$area = $height * $width;
+echo $area;
+?>
