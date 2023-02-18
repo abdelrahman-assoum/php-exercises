0b9ff25b5435e7480474b1d1ff8f2740e179941b
abdelrahman <abdelrahman.assoum@gmail.com>
2023-02-18 17:41:12 +0200

    finish Step_5

---
+++ php-basics-01/Step_5.php
@@ -0,0 +1,10 @@
+<?php 
+$course_name = "flutter complete course";
+$enrolled_students = 200;
+$price = 49.99;
+$on_discount = true;
+echo "Course title: " . $course_name . "\n";
+echo "Enrolled Students: " . $enrolled_students . "\n";
+echo "Course price: " . $price . "\n";
+echo "Course on discount: " . ($on_discount ? "Yes" : "No") . "\n";
+?>
